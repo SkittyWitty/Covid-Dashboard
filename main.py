fb12b7d0faa50abea8294be2b8a38390272c400f
Mindy DeWaal <kawiititan@gmail.com>
2022-02-27 18:34:41 -0800

    Entire dictionary pre-calculated. Too slow.

--- main.py
+++ main.py
@@ -2,17 +2,15 @@ import streamlit as st
 import pandas as pd
 import datetime as dt
 import plotly.express as px
+import plotly
 
 # imports to support opening and reading USA county geojson data
 from urllib.request import urlopen
 import json
 
-# Load in all datasets
+# Constants
 PATH = "./"
-confirmed_cases  = pd.read_csv(PATH + "covid_confirmed_usafacts.csv", dtype={"fips": str})
-deaths = pd.read_csv(PATH + "covid_deaths_usafacts.csv")
-# General population numbers of each county in the USA
-county_population = pd.read_csv(PATH + "covid_county_population_usafacts.csv")
+SUNDAY = 6 # Integer the datetime functionaility associates with Sunday
 
 # Column Labels
 STATE       = "State"
@@ -24,12 +22,17 @@ DATE = "Date"
 WEEK_DATE = "Week Date"
 FIPS = "fips"
 
-# Constants
-SUNDAY = 6 # Integer the datetime functionaility associates with Sunday
+# Load in all datasets
+confirmed_cases = pd.read_csv(PATH + "covid_confirmed_usafacts.csv")
+deaths          = pd.read_csv(PATH + "covid_deaths_usafacts.csv")
+# General population numbers of each county in the USA
+county_population = pd.read_csv(PATH + "covid_county_population_usafacts.csv")
+
 
 # Common Functions
 # A week is defined as starting on Sunday and ending on Saturday
 week_start = lambda row: row[DATE] if (row[DATE].weekday() == SUNDAY) else row[DATE] - dt.timedelta(days=row[DATE].weekday() + 1)
+county_id_tranfrom = lambda row: "0" + row[COUNTY_ID] if (len(row[COUNTY_ID]) == 4) else row[COUNTY_ID]
 
 # Begin displaying data
 st.title("COVID Dashboard")
@@ -52,6 +55,7 @@ new_case_sum["Week Date"] = new_case_sum.apply(week_start, axis=1)
 # Cut out weeks that do not have the full 7 days accounted for
 week_day_count = new_case_sum.groupby(["Week Date"]).count()
 new_case_short_weeks = week_day_count[week_day_count[DATE] < 7].index.tolist()
+new_case_weeks = week_day_count[week_day_count[DATE] == 7].index.tolist()
 
 new_case_sum = new_case_sum.groupby(["Week Date"]).sum()
 new_case_sum = new_case_sum.drop(new_case_short_weeks)
@@ -98,41 +102,76 @@ with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-c
 # Week (Sunday Date)| County | Number of new cases that week / 100,000 | County's total population (will give input on the color the county should be)
 weekly_county_cases = confirmed_cases.drop([COUNTY_NAME, STATE, STATE_ID], axis=1)
 weekly_county_cases = weekly_county_cases[weekly_county_cases[COUNTY_ID] > 0] # Removing State Unallocated areas unable to properly render on map
+
+# For now let's work with a subset of counties !!!!!!!!!!!!
+weekly_county_cases = weekly_county_cases[(weekly_county_cases[COUNTY_ID] < 7000)]
+
+# Adding a "0" to COUNTY_ID's that are too short
+weekly_county_cases[COUNTY_ID] = weekly_county_cases[COUNTY_ID].astype(str)
+weekly_county_cases[COUNTY_ID] = weekly_county_cases.apply(county_id_tranfrom, axis=1)
+
 dates = weekly_county_cases.keys().tolist()
 dates.remove(COUNTY_ID) # removing County FIPS from the list
-week_day_count = death_sum.groupby(["Week Date"]).count()
+week_day_count = death_sum.groupby([WEEK_DATE]).count()
 
-# For now let's work with a subset of counties
-#weekly_county_cases = weekly_county_cases[weekly_county_cases[COUNTY_ID] < 2000]
-
+# If county FIPS only has 4 characters add a "0" to the beginning
 weekly_county_cases = weekly_county_cases.melt(id_vars=[COUNTY_ID], value_vars=dates, var_name=DATE, value_name="cases")
 weekly_county_cases[DATE] = pd.to_datetime(weekly_county_cases[DATE])
-weekly_county_cases[WEEK_DATE] = weekly_county_cases.apply(week_start, axis=1)
-#weekly_county_cases[WEEK_DATE] = pd.to_datetime(weekly_county_cases[WEEK_DATE])
-weekly_county_cases = weekly_county_cases.rename(columns={ COUNTY_ID : FIPS })
-# Retain Week Date and County FIPS. Summing all days that have the same week date
-weekly_county_cases = weekly_county_cases.groupby([FIPS, WEEK_DATE]).sum()
+
+# Maybe here the user can select a week from the previous list of weeks that have been calculated
+# We can backwards calculate the days for that week.
+# Than select 
+
+# Calculating the week start date of each date
+weekly_county_cases[WEEK_DATE] = weekly_county_cases.apply(week_start, axis=1) # This function takes forever
+
+# Retain Week Date and County FIPS. Summing over all cases that have the same week date
+weekly_county_cases = weekly_county_cases.groupby([COUNTY_ID, WEEK_DATE]).sum()
 
 # Remove short weeks
 weekly_county_cases = weekly_county_cases.drop(index=new_case_short_weeks, level=1)
 weekly_county_cases = weekly_county_cases.reset_index()
-test_date = pd.Timestamp("2020-01-26")
-st.write(test_date)
-meh = weekly_county_cases[weekly_county_cases[WEEK_DATE] == test_date]
-meh
-fig = px.choropleth(meh, geojson=counties, locations='fips', color='cases',
-                           color_continuous_scale="Viridis",
-                           range_color=(0, 12),
-                           scope="usa",
-                           labels={'cases':'Weekly Cases'})
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-fig.show()
+week_dates = list(dict.fromkeys(weekly_county_cases[WEEK_DATE]))
+
+# per 100,000 people
+weekly_county_cases["cases"] = weekly_county_cases["cases"].floordiv(100000)
+
+# Create dictionary of all choropleth's
+meh = []
+for i in range(len(week_dates)):
+    test_date = week_dates[i]
+    test_thing = weekly_county_cases[weekly_county_cases[WEEK_DATE] == test_date]
+    meh.append(
+        dict(type='choropleth',
+             locations = test_thing[COUNTY_ID],
+             z=test_thing["cases"],
+             geojson=counties))
+
+meh[0]
+
+# Slider using all the possible weeks to select from
+steps = []
+for i in range(len(week_dates)):
+    step = dict(method='restyle',
+                args=['visible', [False] * len(week_dates)],
+                label=week_dates[i].strftime("%B %d, %Y"))
+    step['args'][1][i] = True
+    steps.append(step)
+
+
+slider = [dict(active=0,
+                pad={"t": 1},
+                steps=steps)]    
+layout = dict(title ='Meh', 
+                geo=dict(scope='usa', projection={'type': 'albers usa'}),
+                sliders=slider)
+  
+fig = dict(data=meh, 
+           layout=layout)
+plotly.offline.iplot(fig)
+
+
 # end region
-
-
-# Notes:
-# Use st.write to view raw data
-
 
 
 # Streamlit widgets automatically run the script from top to bottom. Since

7bec220b7cec3d6c1ffce24f9e94b5e4fa1fd36e
Mindy DeWaal <kawiititan@gmail.com>
2022-04-09 23:00:47 -0700

    Finished >.<

--- covidDashboard.py
+++ covidDashboard.py
@@ -1,14 +1,14 @@
+# FOLDER WHERE DATASET FILES ARE LOCATED
+PATH = "./"
+
 import streamlit as st
 import pandas as pd
-from covidDashboardPrep import calculatePerCapita, scrubUnallocated, weeklySumsPerCounty, totalWeeklySums, validDates, trimDays
+from covidDashboardPrep import calculatePerCapita, scrubData, weeklySumsPerCounty, totalWeeklySums, validDates, trimDays
 import plotly.express as px
 
 # imports to support opening and reading USA county geojson data
 from urllib.request import urlopen
 import json
-
-# Constants
-PATH = "./"
 
 # Column Labels
 STATE       = "State"
@@ -17,8 +17,6 @@ STATE_ID    = "StateFIPS"
 COUNTY_ID   = "countyFIPS"
 POPULATION  = "population"
 DATE        = "Date"
-WEEK_DATE   = "Week Date"
-FIPS        = "fips"
 DEATHS      = "Deaths"
 CASES       = "Cases"
 
@@ -37,11 +35,9 @@ with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-c
     counties = json.load(response)
 
 # region Q1
-# Produce a line plot of the weekly new cases of Covid-19 in the USA from the
-# start of the pandemic. Weeks start on Sunday and end on Saturday. Only include full weeks
-
-# Retrieve a list of dates for week starts and days in the data
 new_weekly_cases_per_county = confirmed_cases.drop([COUNTY_ID, COUNTY_NAME, STATE, STATE_ID], axis=1)
+
+# Assuming that days for covid cases and deaths are the same.
 start_week_list, all_days_list = validDates(new_weekly_cases_per_county)
 
 new_weekly_cases_per_county = trimDays(new_weekly_cases_per_county, all_days_list)
@@ -52,8 +48,6 @@ new_weekly_cases = totalWeeklySums(new_weekly_cases, start_week_list, confirmed_
 # endregion
 
 # region Q2
-# Produce a line plot of the weekly deaths due to Covid-19 in the USA from the
-# start of the pandemic. Weeks start on Sunday and end on Saturday. Only include full weeks.
 new_weekly_deaths_per_county = deaths.drop([COUNTY_ID, COUNTY_NAME, STATE, STATE_ID], axis=1)
 
 new_weekly_deaths_per_county = trimDays(new_weekly_deaths_per_county, all_days_list)
@@ -64,12 +58,20 @@ new_weekly_deaths = totalWeeklySums(new_weekly_deaths, start_week_list, deaths[C
 # endregion
 
 # region Prepare County Population Data
+
 county_population = county_population.drop([COUNTY_NAME, STATE], axis=1)
+# Remove Unallocated Region by filtering County FIPS
 county_population = county_population[county_population[COUNTY_ID] > 0]
+# Remove Counties with no population
 county_population = county_population[county_population[POPULATION] > 0]
 
+# Issue with some County FIPS having less than 4 numbers in our dataset
+# While the geojson used to locate the counties has 5 numbers in the County FIPS reguardless if they start with 0. 
+# Treat County FIPS as a string
 county_population = county_population.astype({COUNTY_ID : str})
+# Add another 0 if County FIPS is short
 county_population.index = county_population[COUNTY_ID].apply(county_id_transform)
+# Dropping Column we peformed operations on as it has already been set as our index
 county_population = county_population.drop(COUNTY_ID, axis=1)
 # endregion
 
@@ -78,45 +80,45 @@ st.title("COVID Dashboard")
 
 # Display Q1
 st.write("New Weekly Cases Trend")
-weekly_cases_per_capita = scrubUnallocated(confirmed_cases, new_weekly_cases_per_county, COUNTY_ID)
+weekly_cases_per_capita = scrubData(confirmed_cases, new_weekly_cases_per_county, COUNTY_ID)
 weekly_cases_per_capita = calculatePerCapita(weekly_cases_per_capita, county_population[POPULATION], CAPITA_GUIDELINES)
-st.write(weekly_cases_per_capita)
 st.line_chart(new_weekly_cases)
 
 # Display Q2
 st.write("Weekly Deaths Trend")
-weekly_deaths_per_capita = scrubUnallocated(deaths, new_weekly_deaths_per_county, COUNTY_ID)
+weekly_deaths_per_capita = scrubData(deaths, new_weekly_deaths_per_county, COUNTY_ID)
 weekly_deaths_per_capita = calculatePerCapita(weekly_cases_per_capita, county_population[POPULATION], CAPITA_GUIDELINES)
 st.line_chart(new_weekly_deaths)
 
 #region Display Choropleth
 def get_map(week, data, title, label, max_range):
     week_case = data[week]
-    st.write(week_case)
+    week_display = week.strftime('%Y-%m-%d')
+
     # Building map figure
     figure = px.choropleth(week_case,
     geojson=counties, locations=data.index,
                                 color=week,
-                                color_continuous_scale="amp",
+                                color_continuous_scale="ylorbr",
                                 range_color=[0, max_range],
                                 scope="usa",
                                 labels={str(week):label})
-    figure.update_layout(title_text=title + " week of " + str(week))
+    figure.update_layout(title_text=title + " week of " + week_display)
     return figure
 
-FIRST_WEEK = start_week_list[0]
-LAST_WEEK = start_week_list[len(start_week_list) - 1]
-
+# Placeholders for where the maps will populate
 with st.spinner('Fetching a new map'):
     cases_map_location = st.empty()
     death_map_location = st.empty()
 
+# Slider to select week
 with st.form("Compute_Values"):
     week = st.select_slider(
         label='Please select a week.',
         options=start_week_list)
     submitted = st.form_submit_button("Update Figure")
 
+# Options to select or auto play the maps
 if st.button("Auto Play"):
     for week in start_week_list:
         cases_map_location.plotly_chart(get_map(week, weekly_cases_per_capita, "Cases's per 100,000", "Cases per 100k", CAPITA_GUIDELINES))
@@ -124,7 +126,6 @@ if st.button("Auto Play"):
 else:
     cases_map_location.plotly_chart(get_map(week, weekly_cases_per_capita, "Cases's per 100,000", "Cases per 100k", CAPITA_GUIDELINES))
     death_map_location.plotly_chart(get_map(week, weekly_deaths_per_capita, "Death's per 100,000", "Deaths per 100k", CAPITA_GUIDELINES))
-
 # endregion
 
 st.button("Re-run")
--- covidDashboardPrep.py
+++ covidDashboardPrep.py
@@ -12,6 +12,16 @@ week_start_date = lambda date: date.name if (date.name.weekday() == SUNDAY) else
 
 # region Get Week Data
 def validDates(data):
+    """
+    desc
+        Calculate what full weeks of data we have and add those to a list of valid days
+    params
+        data - dataframe to perform operations on
+    return 
+        start_week_list - list of the starting Sunday's
+        day_list - list of days that belong to a full week of data
+    """
+
     day_list = data.copy()
     day_list = pd.to_datetime(data.columns).to_frame(name=DATE)
 
@@ -29,6 +39,14 @@ def validDates(data):
     return start_week_list, day_list
 
 def trimDays(data, day_list):
+    """
+    desc
+        Trims days from given dataframe that do not belong to a full week.
+    params
+        data - dataframe to perform operations on
+    return 
+        data - the dataframe with the given dates removed
+    """
     first_day = day_list[0].weekday()
     last_index = len(day_list) - 1
     last_day = day_list[last_index].weekday()
@@ -45,28 +63,63 @@ def trimDays(data, day_list):
 
     return data
 
-def weeklySumsPerCounty(new_weekly, start_week_list):
-    m = len(new_weekly.columns)
+def weeklySumsPerCounty(data, start_week_list):
+    """
+    desc
+        Calculates a summation of data gathered by week for each county
+    params
+        data - dataframe with days of data
+        start_week_list - list of days that represent the start of a week 
+    return 
+        weekly - the dataframe with every week grouped together with a cumulative
+                 subtraction peformed
+    """
+    m = len(data.columns)
 
+    # Subtract each column from the column previous prior
     for i in reversed(range(m)):
         if(i != 0):
-            new_weekly.iloc[:, i] = new_weekly.iloc[:, i] - new_weekly.iloc[:, i-1]
-            new_weekly.iloc[:, i] = new_weekly.iloc[:, i].clip(0)
+            data.iloc[:, i] = data.iloc[:, i] - data.iloc[:, i-1]
+            data.iloc[:, i] = data.iloc[:, i].clip(0)
 
-    new_weekly = new_weekly.groupby([i//7 for i in range(0, m)], axis = 1).sum()
-    new_weekly.columns = start_week_list
+    # Groups columns in blocks of 7 and sums their contents
+    data = data.groupby([i//7 for i in range(0, m)], axis = 1).sum()
+    # Restore the names of the columns
+    data.columns = start_week_list
 
-    return new_weekly
+    return data
 
-def totalWeeklySums(total_weekly, start_week_list, replace_data, replace_id, new_label):
-    total_weekly[replace_id] = replace_data
-    total_weekly = total_weekly.melt(id_vars=[replace_id], value_vars=start_week_list, var_name=DATE, value_name=new_label)
-    total_weekly = total_weekly.drop([replace_id], axis = 1)
+def totalWeeklySums(total_weekly, start_week_list, var_data, id_var, new_label):
+    """
+    desc
+        Calculates a summation of data for each week
+    params
+        total_weekly - Calculated total for each 
+        start_week_list - list of the starting Sunday's
+        var_data - variable data to be used in the melting process
+        id_var - ID variable to perform melt with 
+        new_label - label for the new column to be created
+    returns
+        weekly - the dataframe with every week grouped together with a cumulative
+                 subtraction peformed
+    """
+    total_weekly[id_var] = var_data
+    total_weekly = total_weekly.melt(id_vars=[id_var], value_vars=start_week_list, var_name=DATE, value_name=new_label)
+    total_weekly = total_weekly.drop([id_var], axis = 1)
     total_weekly = total_weekly.groupby([DATE]).sum()
 
     return total_weekly
 
-def scrubUnallocated(raw_data, data, id_label):
+def scrubData(raw_data, data, id_label):
+    """
+    desc
+        removes data less than 0 in the specified ID label in the data frame
+    params
+        data - dataframe to perform operations on
+        id_label - the id to replace to be checked for the less than 0 condition
+    return 
+        data - the dataframe with only values greater than 0 for given the ID label
+    """
     data[id_label] = raw_data[id_label]
     data = data[data[id_label] > 0]
     data.index = data[id_label]
@@ -76,9 +129,19 @@ def scrubUnallocated(raw_data, data, id_label):
 
 # region Q3, Q4 per capita calculations
 # Using Plotly Choropleth map produce a map of the USA displaying for each county the new 
-# cases of covid per 100,000 people in a week.
+# cases of and deaths from, covid per 100,000 people in a week.
 @st.experimental_memo
 def calculatePerCapita(data, pop_data, capita_guidelines=100000):
+    """
+    desc
+        Calculates per capita measurement 
+    params
+        data - dataframe to perform operations on
+        pop_data - dataframe containing population data
+        capita_guidelines - the calculations to be made per capita default to 100,000
+    return 
+        data - the dataframe with the per capita calculations performed
+    """
     data.index = pop_data.index
     data = (data * (capita_guidelines))
     data = data.div(pop_data, axis=0).fillna(0)
